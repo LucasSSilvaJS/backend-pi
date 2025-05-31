c653cd8ca14719b03c9e95b54edbcd406dc4a57d
Maria Nascimento <melonascmento@gmail.com>
2025-05-31 01:00:56 -0300

    app atualizado

--- router/odontograma.router.js
+++ router/odontograma.router.js
@@ -20,22 +20,3 @@ router.route('/:id')
     .delete(deleteOdontograma);
 
 export default router;
-//     return res.status(404).json({ mensagem: "Odontograma não encontrado" });
-//     }
-//     res.status(200).json({ mensagem: "Odontograma deletado com sucesso" });
-//   } catch (erro) {
-//     res.status(500).json({ mensagem: "Erro ao deletar odontograma", erro });
-//   }
-//   }
-// };
-// Compare this snippet from routes/odontograma.routes.js:
-// import express from "express";
-// import { createOdontograma, getAllOdontogramas, getOdontogramaById, updateOdontograma, deleteOdontograma } from "../controllers/odontograma.controller.js";
-//
-// const router = express.Router();
-//
-// router.route('/')
-//     .get(getAllOdontogramas)
-//     .post(createOdontograma);
-//
-// router.route('/:id')
