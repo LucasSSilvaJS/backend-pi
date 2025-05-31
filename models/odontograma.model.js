4a834a54e176a959d9dab1c58509e171d4e1d373
Maria Nascimento <melonascmento@gmail.com>
2025-05-30 22:26:05 -0300

    teste

--- models/odontograma.model.js
+++ models/odontograma.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
+//Comentário teste incluir lista de dentes e commentário de observação
 const OdontogramaSchema = mongoose.Schema({
-    numero: { type: Number, required: true },
+    dentes: [{
+    identificacao: { type: Number, required: true },
     observacao: { type: String, required: false }
+    }]
 });
 
 const Odontograma = mongoose.model('Odontograma', OdontogramaSchema);
 
 export default Odontograma;
-
