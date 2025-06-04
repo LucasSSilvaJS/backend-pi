92b02396bb4bb5aae2512cb85e8d176b7f7fb4ec
LucasSSilvaJs <lucas25th@gmail.com>
2025-06-04 00:58:41 -0300

    mudança swagger

--- docs/swagger.js
+++ docs/swagger.js
@@ -30,49 +30,63 @@ const options = {
                 Vitima: {
                     type: "object",
                     properties: {
+                        _id: {
+                            type: "string",
+                            description: "ID da vítima",
+                            example: "627b6a9c4f6f6e62d5c7b6a9"
+                        },
                         nic: {
                             type: "string",
-                            description: "N mero de Identifica o Civil",
+                            description: "Número de Identificação Civil",
                             example: "12345678",
+                            nullable: true
                         },
                         nome: {
                             type: "string",
-                            description: "Nome da vitima",
+                            description: "Nome da vítima",
                             example: "Fulano de Tal",
+                            nullable: true
                         },
                         genero: {
                             type: "string",
-                            description: "G nero da vitima (M/F)",
+                            description: "Gênero da vítima (M/F)",
                             example: "M",
+                            nullable: true
                         },
                         idade: {
-                            type: "integer",
-                            description: "Idade da vitima",
+                            type: "number",
+                            description: "Idade da vítima",
                             example: 25,
+                            nullable: true
                         },
                         documento: {
                             type: "string",
-                            description: "Documento da vitima (RG/CPF)",
+                            description: "Documento da vítima (RG/CPF) - Único",
                             example: "123456789",
+                            nullable: true,
+                            unique: true
                         },
                         endereco: {
                             type: "string",
-                            description: "Endere o da vitima",
+                            description: "Endereço da vítima",
                             example: "Rua das Flores, 123",
+                            nullable: true
                         },
                         corEtnia: {
                             type: "string",
-                            description: "Cor/Etnia da vitima",
+                            description: "Cor/Etnia da vítima",
                             example: "Branca",
+                            nullable: true
                         },
                         odontograma: {
                             type: "array",
                             items: {
-                                $ref: "#/components/schemas/Odontograma",
+                                $ref: "#/components/schemas/Odontograma"
                             },
-                            description: "Odontograma(s) relacionado(s)   vitima",
-                        },
-                    },
+                            description: "Odontograma(s) relacionado(s) à vítima",
+                            nullable: true
+                        }
+                    }
                 },
                 User: {
                     type: "object",
@@ -208,31 +222,31 @@ const options = {
                         _id: {
                             type: "string",
                             description: "ID do odontograma",
-                            example: "627b6a9c4f6f6e62d5c7b6a9",
+                            example: "627b6a9c4f6f6e62d5c7b6a9"
                         },
                         identificacao: {
                             type: "number",
-                            description: "Identifica o do odontograma",
-                            example: 1,
+                            description: "Identificação do odontograma",
+                            example: 1
                         },
                         observacao: {
                             type: "string",
-                            description: "Observa o do odontograma",
-                            example: "Observa o do odontograma",
+                            description: "Observação do odontograma",
+                            example: "Observação do odontograma"
                         },
                         createdAt: {
                             type: "string",
                             format: "date-time",
-                            description: "Data de cria o do odontograma",
-                            example: "2022-01-01T12:00:00.000Z",
+                            description: "Data de criação do odontograma",
+                            example: "2022-01-01T12:00:00.000Z"
                         },
                         updatedAt: {
                             type: "string",
                             format: "date-time",
-                            description: "Data de atualiza o do odontograma",
-                            example: "2022-01-01T12:00:00.000Z",
-                        },
-                    },
+                            description: "Data de atualização do odontograma",
+                            example: "2022-01-01T12:00:00.000Z"
+                        }
+                    }
                 },
                 Laudo: {
                     type: "object",
@@ -295,77 +309,87 @@ const options = {
                 Evidencia: {
                     type: "object",
                     properties: {
+                        _id: {
+                            type: "string",
+                            description: "ID da evidência",
+                            example: "627b6a9c4f6f6e62d5c7b6a9"
+                        },
                         tipo: {
                             type: "string",
                             description: "Tipo da evidência",
-                            example: "FOTO",
+                            example: "FOTO"
                         },
                         dataColeta: {
                             type: "string",
                             format: "date-time",
                             description: "Data de coleta da evidência",
-                            example: "2022-01-01T00:00:00.000Z",
+                            example: "2022-01-01T00:00:00.000Z"
                         },
                         status: {
                             type: "string",
                             description: "Status da evidência",
                             enum: ["Em análise", "Concluído"],
                             example: "Em análise",
+                            default: "Em análise"
                         },
                         coletadaPor: {
                             type: "string",
                             description: "ID do usuário que coletou a evidência",
-                            example: "627b6a9c4f6f6e62d5c7b6a9",
+                            example: "627b6a9c4f6f6e62d5c7b6a9"
                         },
                         geolocalizacao: {
                             type: "object",
+                            required: true,
                             properties: {
                                 latitude: {
                                     type: "string",
                                     description: "Latitude da localização da evidência",
-                                    example: "-23.555555",
+                                    example: "-23.555555"
                                 },
                                 longitude: {
                                     type: "string",
                                     description: "Longitude da localização da evidência",
-                                    example: "-46.645645",
-                                },
-                            },
+                                    example: "-46.645645"
+                                }
+                            }
                         },
                         imagens: {
                             type: "array",
                             items: {
                                 type: "string",
                                 description: "ID da imagem de evidência",
-                                example: "627b6a9c4f6f6e62d5c7b6a9",
+                                example: "627b6a9c4f6f6e62d5c7b6a9"
                             },
+                            minItems: 1
                         },
                         textos: {
                             type: "array",
                             items: {
                                 type: "string",
                                 description: "ID do texto de evidência",
-                                example: "627b6a9c4f6f6e62d5c7b6a9",
+                                example: "627b6a9c4f6f6e62d5c7b6a9"
                             },
+                            minItems: 1
                         },
                         laudo: {
                             type: "string",
                             description: "ID do laudo associado à evidência",
                             example: "627b6a9c4f6f6e62d5c7b6a9",
+                            nullable: true
                         },
                         createdAt: {
                             type: "string",
                             format: "date-time",
                             description: "Data de criação da evidência",
-                            example: "2022-01-01T12:00:00.000Z",
+                            example: "2022-01-01T12:00:00.000Z"
                         },
                         updatedAt: {
                             type: "string",
                             format: "date-time",
                             description: "Data de atualização da evidência",
-                            example: "2022-01-01T12:00:00.000Z",
-                        },
-                    },
+                            example: "2022-01-01T12:00:00.000Z"
+                        }
+                    }
                 },
                 Caso: {
                     type: "object",
